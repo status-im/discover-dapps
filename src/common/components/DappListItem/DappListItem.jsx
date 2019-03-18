4ab58a9f5e945111a653e88817f14b87ded0bd36
jamesgarethsmith <james.gareth.smith@gmail.com>
2019-03-18 10:07:51 +0200

    style: font setup

--- src/common/components/DappListItem/DappListItem.jsx
+++ src/common/components/DappListItem/DappListItem.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { DappModel } from '../../utils/models'
+import styles from './DappListItem.module.scss'
 
 const DappListItem = props => {
   const { name } = props
-  return <div>{name}</div>
+  return <div className={styles.test}>{name}</div>
 }
 
 DappListItem.propTypes = DappModel
