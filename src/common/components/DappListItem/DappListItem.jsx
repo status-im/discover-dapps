9f519e7b687fd429aebb4b836850acac2dceb9a6
jamesgarethsmith <james.gareth.smith@gmail.com>
2019-03-18 11:36:38 +0200

    fix: fonts moved to public folder

--- src/common/components/DappListItem/DappListItem.jsx
+++ src/common/components/DappListItem/DappListItem.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import { DappModel } from '../../utils/models'
 import styles from './DappListItem.module.scss'
+import icon from '../../assets/images/icon.svg'
 
 const DappListItem = props => {
-  const { name } = props
-  return <div className={styles.test}>{name}</div>
+  const { name, description, url } = props
+  return (
+    <div className={styles.listItem}>
+      <div>
+        <img src={icon} alt="App icon" />
+      </div>
+      <div>
+        <h2>{name}</h2>
+        <p>{description}</p>
+        <a href={url}>{url}</a>
+      </div>
+    </div>
+  )
 }
 
 DappListItem.propTypes = DappModel
