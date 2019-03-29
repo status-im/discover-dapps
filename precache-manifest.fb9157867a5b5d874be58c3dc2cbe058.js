5cf706235ba87b0449e3a90b40b7f7e8fb280c86
Andy Tudhope <tuddy0525@gmail.com>
2019-03-29 09:25:02 +0200

    Updates

---
+++ precache-manifest.fb9157867a5b5d874be58c3dc2cbe058.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "59b137420d107deb1bd1c42a04257fdf",
+    "url": "/discover/static/media/exchanges.59b13742.svg"
+  },
+  {
+    "revision": "5b9e96647acc62fb66fe",
+    "url": "/discover/static/css/main.bf4492c1.chunk.css"
+  },
+  {
+    "revision": "454f8fcbf033e5a16979",
+    "url": "/discover/static/js/runtime~main.57338630.js"
+  },
+  {
+    "revision": "77521551f2b2f62063c0",
+    "url": "/discover/static/js/2.d5151aa4.chunk.js"
+  },
+  {
+    "revision": "9170c622c96e1bdd8deed5dc8ceb4a4f",
+    "url": "/discover/static/media/other.9170c622.svg"
+  },
+  {
+    "revision": "6d55f68a34012512076271cf29c709a0",
+    "url": "/discover/static/media/icon.6d55f68a.svg"
+  },
+  {
+    "revision": "5b9e96647acc62fb66fe",
+    "url": "/discover/static/js/main.7ebe0f90.chunk.js"
+  },
+  {
+    "revision": "964ef8e979606df5b9fc6ff216fcf3d0",
+    "url": "/discover/static/media/marketplaces.964ef8e9.svg"
+  },
+  {
+    "revision": "5f2fc3ba1ac7649afd6faac10a3ac760",
+    "url": "/discover/static/media/games.5f2fc3ba.svg"
+  },
+  {
+    "revision": "48f1d6ea749d19b38883a0f56d88783a",
+    "url": "/discover/static/media/collectibles.48f1d6ea.svg"
+  },
+  {
+    "revision": "f0cf579541f81ddf3561cb4c73fd8128",
+    "url": "/discover/static/media/social-networks.f0cf5795.svg"
+  },
+  {
+    "revision": "624c69f960ad47f8f217ac2e1cea663f",
+    "url": "/discover/static/media/utilities.624c69f9.svg"
+  },
+  {
+    "revision": "491e48708a9cd9d2f7179cc778940674",
+    "url": "/discover/index.html"
+  }
+];
