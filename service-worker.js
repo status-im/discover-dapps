944f86a786637c66d645831b838a3804a30e182f
Andy Tudhope <tuddy0525@gmail.com>
2019-03-29 09:29:44 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/discover/precache-manifest.fb9157867a5b5d874be58c3dc2cbe058.js"
+  "/discover-dapps/precache-manifest.9e252c6c9e5b18850c2b49bb02fc9f13.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/discover/index.html", {
+workbox.routing.registerNavigationRoute("/discover-dapps/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
