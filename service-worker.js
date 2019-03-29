e4032a7ba1beda2495360d2baf0490980fb38e9f
Andy Tudhope <tuddy0525@gmail.com>
2019-03-29 10:33:21 +0200

    More tests

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/discover-dapps/precache-manifest.9e252c6c9e5b18850c2b49bb02fc9f13.js"
+  "/precache-manifest.9e252c6c9e5b18850c2b49bb02fc9f13.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/discover-dapps/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
