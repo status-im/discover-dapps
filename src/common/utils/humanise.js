4828eab27f735c860fe751c20b67914246650f97
James Gareth Smith <james.gareth.smith@gmail.com>
2019-03-29 10:32:01 +0200

    feat: basic scaffold

--- src/common/utils/humanise.js
+++ src/common/utils/humanise.js
@@ -1,7 +1,12 @@
-const humanise = (value, joiner = ' ') =>
-  value
+const humanise = (value, joiner = ' ') => {
+  if (!value) {
+    return ''
+  }
+
+  return value
     .split('_')
     .map(word => `${word[0]}${word.slice(1).toLowerCase()}`)
     .join(joiner)
+}
 
 export default humanise
--- src/modules/CategorySelector/CategorySelector.jsx
+++ src/modules/CategorySelector/CategorySelector.jsx
@@ -1,21 +1,75 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import categories from '../../common/utils/categories'
+import humanise from '../../common/utils/humanise'
+import dropdownArrows from '../../common/assets/images/dropdown-arrows.svg'
 
-const CategorySelector = props => {
-  const { category, select } = props
-  const handleChange = event => select(event.target.value)
+// const CategorySelector = props => {
+//   const { category, select } = props
+//   const handleChange = event => select(event.target.value)
 
-  return (
-    <select value={category || ''} onChange={handleChange}>
-      <option style={{ display: 'none' }} />
-      {categories.map(c => (
-        <option key={c.key} value={c.key}>
-          {c.value}
-        </option>
-      ))}
-    </select>
-  )
+//   return (
+//     <select value={category || ''} onChange={handleChange}>
+//       <option style={{ display: 'none' }} />
+//       {categories.map(c => (
+//         <option key={c.key} value={c.key}>
+//           {c.value}
+//         </option>
+//       ))}
+//     </select>
+//   )
+// }
+
+class CategorySelector extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { open: false }
+    this.toggle = this.toggle.bind(this)
+    this.updateCategory = this.updateCategory.bind(this)
+  }
+
+  updateCategory(event) {
+    const { select } = this.props
+    select(event.target.value)
+    this.setState({ open: false })
+  }
+
+  toggle() {
+    const { open } = this.state
+    this.setState({ open: !open })
+  }
+
+  render() {
+    const { open } = this.state
+    const { category } = this.props
+
+    return (
+      <>
+        {open && (
+          <div>
+            {categories.map(c => (
+              <button
+                key={c.key}
+                type="button"
+                value={c.key}
+                onClick={this.updateCategory}
+              >
+                {c.value}
+              </button>
+            ))}
+          </div>
+        )}
+
+        {!open && (
+          <button type="button" onClick={this.toggle}>
+            {/* TODO: category icon here */}
+            <p>{humanise(category)}</p>
+            <img src={dropdownArrows} alt="Toggle category selector" />
+          </button>
+        )}
+      </>
+    )
+  }
 }
 
 CategorySelector.propTypes = {
