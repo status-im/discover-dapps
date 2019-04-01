fdd6fc612bd38ecb887658a7d8d722b0e9617e06
James Gareth Smith <james.gareth.smith@gmail.com>
2019-04-01 18:31:15 +0200

    Footer (#9)
    * style: dummied up footer
    
    * chore: alt text

--- src/modules/Footer/Footer.jsx
+++ src/modules/Footer/Footer.jsx
@@ -1,6 +1,40 @@
 import React from 'react'
 import styles from './Footer.module.scss'
+import icon from '../../common/assets/images/icon.svg'
 
-const Footer = () => <div className={styles.footer}>Footer</div>
+const Footer = () => (
+  <div className={styles.footer}>
+    <div className={styles.footerItem}>
+      <img src={icon} alt="Join the DApp community chat" />
+      <div>
+        <h2>Join the DApp community chat</h2>
+        <p>
+          Status is a community of people all over the world connected by a set
+          of values and principles.
+        </p>
+      </div>
+    </div>
+    <div className={styles.footerItem}>
+      <img src={icon} alt="Submit a DApp" />
+      <div>
+        <h2>Submit a DApp</h2>
+        <p>
+          Status is a community of people all over the world connected by a set
+          of values and principles.
+        </p>
+      </div>
+    </div>
+    <div className={styles.footerItem}>
+      <img src={icon} alt="Support" />
+      <div>
+        <h2>Support</h2>
+        <p>
+          Status is a community of people all over the world connected by a set
+          of values and principles.
+        </p>
+      </div>
+    </div>
+  </div>
+)
 
 export default Footer
