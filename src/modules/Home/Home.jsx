04f98428d836491ceeddb6f13b7b6974a18b9df1
James Gareth Smith <james.gareth.smith@gmail.com>
2019-04-03 12:12:45 +0200

    Home page load (#12)
    * style: loading animation for homepage
    
    * fix: bug with category seletor

--- src/modules/Home/Home.jsx
+++ src/modules/Home/Home.jsx
@@ -2,11 +2,38 @@ import React from 'react'
 import RecentlyAdded from '../RecentlyAdded'
 import Categories from '../Categories'
 import Footer from '../Footer'
+import styles from './Home.module.scss'
 
-export default () => (
-  <>
-    <Categories />
-    <RecentlyAdded />
-    <Footer />
-  </>
-)
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.startLoadingAnimation = this.startLoadingAnimation.bind(this)
+    this.state = { didMount: false }
+  }
+
+  componentDidMount() {
+    setTimeout(this.startLoadingAnimation, 0)
+  }
+
+  startLoadingAnimation() {
+    this.setState({ didMount: true })
+  }
+
+  render() {
+    const { didMount } = this.state
+
+    return (
+      <div
+        className={
+          didMount ? [styles.fade, styles.loaded].join(' ') : styles.fade
+        }
+      >
+        <Categories />
+        <RecentlyAdded />
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default Home
