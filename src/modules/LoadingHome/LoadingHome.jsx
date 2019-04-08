d4fedc6b2d20f40252dd7bff61289788fd739dd6
James Gareth Smith <james.gareth.smith@gmail.com>
2019-04-08 11:57:25 +0200

    style: desktop loader

--- src/modules/LoadingHome/LoadingHome.jsx
+++ src/modules/LoadingHome/LoadingHome.jsx
@@ -23,8 +23,86 @@ const MobileLoader = () => (
 )
 
 const DesktopLoader = () => (
-  <ContentLoader width={320} height={635}>
-    <rect x="15" y="15" rx="5" ry="5" width="90" height="110" />
+  <ContentLoader width={1700} height={2000}>
+    <rect x="15" y="50" rx="5" ry="5" width="200" height="85" />
+    <rect x="223" y="50" rx="5" ry="5" width="200" height="85" />
+    <rect x="431" y="50" rx="5" ry="5" width="200" height="85" />
+    <rect x="639" y="50" rx="5" ry="5" width="200" height="85" />
+    <rect x="847" y="50" rx="5" ry="5" width="200" height="85" />
+    <rect x="1055" y="50" rx="5" ry="5" width="200" height="85" />
+    <rect x="1263" y="50" rx="5" ry="5" width="200" height="85" />
+
+    <circle cx="35" cy="240" r="20" />
+    <rect x="70" y="220" rx="5" ry="5" width="325" height="88" />
+    <circle cx="455" cy="240" r="20" />
+    <rect x="500" y="220" rx="5" ry="5" width="325" height="88" />
+    <circle cx="885" cy="240" r="20" />
+    <rect x="920" y="220" rx="5" ry="5" width="325" height="88" />
+    <circle cx="1305" cy="240" r="20" />
+    <rect x="1340" y="220" rx="5" ry="5" width="325" height="88" />
+
+    <circle cx="35" cy="368" r="20" />
+    <rect x="70" y="348" rx="5" ry="5" width="325" height="88" />
+    <circle cx="455" cy="368" r="20" />
+    <rect x="500" y="348" rx="5" ry="5" width="325" height="88" />
+    <circle cx="885" cy="368" r="20" />
+    <rect x="920" y="348" rx="5" ry="5" width="325" height="88" />
+    <circle cx="1305" cy="368" r="20" />
+    <rect x="1340" y="348" rx="5" ry="5" width="325" height="88" />
+
+    <circle cx="35" cy="496" r="20" />
+    <rect x="70" y="476" rx="5" ry="5" width="325" height="88" />
+    <circle cx="455" cy="496" r="20" />
+    <rect x="500" y="476" rx="5" ry="5" width="325" height="88" />
+    <circle cx="885" cy="496" r="20" />
+    <rect x="920" y="476" rx="5" ry="5" width="325" height="88" />
+    <circle cx="1305" cy="496" r="20" />
+    <rect x="1340" y="476" rx="5" ry="5" width="325" height="88" />
+
+    <circle cx="35" cy="624" r="20" />
+    <rect x="70" y="604" rx="5" ry="5" width="325" height="88" />
+    <circle cx="455" cy="624" r="20" />
+    <rect x="500" y="604" rx="5" ry="5" width="325" height="88" />
+    <circle cx="885" cy="624" r="20" />
+    <rect x="920" y="604" rx="5" ry="5" width="325" height="88" />
+    <circle cx="1305" cy="624" r="20" />
+    <rect x="1340" y="604" rx="5" ry="5" width="325" height="88" />
+
+    <circle cx="35" cy="752" r="20" />
+    <rect x="70" y="732" rx="5" ry="5" width="325" height="88" />
+    <circle cx="455" cy="752" r="20" />
+    <rect x="500" y="732" rx="5" ry="5" width="325" height="88" />
+    <circle cx="885" cy="752" r="20" />
+    <rect x="920" y="732" rx="5" ry="5" width="325" height="88" />
+    <circle cx="1305" cy="752" r="20" />
+    <rect x="1340" y="732" rx="5" ry="5" width="325" height="88" />
+
+    <circle cx="35" cy="880" r="20" />
+    <rect x="70" y="860" rx="5" ry="5" width="325" height="88" />
+    <circle cx="455" cy="880" r="20" />
+    <rect x="500" y="860" rx="5" ry="5" width="325" height="88" />
+    <circle cx="885" cy="880" r="20" />
+    <rect x="920" y="860" rx="5" ry="5" width="325" height="88" />
+    <circle cx="1305" cy="880" r="20" />
+    <rect x="1340" y="860" rx="5" ry="5" width="325" height="88" />
+
+    <circle cx="35" cy="1008" r="20" />
+    <rect x="70" y="988" rx="5" ry="5" width="325" height="88" />
+    <circle cx="455" cy="1008" r="20" />
+    <rect x="500" y="988" rx="5" ry="5" width="325" height="88" />
+    <circle cx="885" cy="1008" r="20" />
+    <rect x="920" y="988" rx="5" ry="5" width="325" height="88" />
+    <circle cx="1305" cy="1008" r="20" />
+    <rect x="1340" y="988" rx="5" ry="5" width="325" height="88" />
+
+    <circle cx="35" cy="1136" r="20" />
+    <rect x="70" y="1116" rx="5" ry="5" width="325" height="88" />
+    <circle cx="455" cy="1136" r="20" />
+    <rect x="500" y="1116" rx="5" ry="5" width="325" height="88" />
+    <circle cx="885" cy="1136" r="20" />
+    <rect x="920" y="1116" rx="5" ry="5" width="325" height="88" />
+    <circle cx="1305" cy="1136" r="20" />
+    <rect x="1340" y="1116" rx="5" ry="5" width="325" height="88" />
   </ContentLoader>
 )
 
