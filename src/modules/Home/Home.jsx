c8b4b29408c1f623acb0b9cf4934c3de96183ab8
James Gareth Smith <james.gareth.smith@gmail.com>
2019-04-08 11:58:24 +0200

    New homepage loader (#13)
    * style: mobile loader
    
    * style: desktop loader

--- src/modules/Home/Home.jsx
+++ src/modules/Home/Home.jsx
@@ -2,36 +2,40 @@ import React from 'react'
 import RecentlyAdded from '../RecentlyAdded'
 import Categories from '../Categories'
 import Footer from '../Footer'
-import styles from './Home.module.scss'
+import LoadingHome from '../LoadingHome'
 
 class Home extends React.Component {
   constructor(props) {
     super(props)
     this.startLoadingAnimation = this.startLoadingAnimation.bind(this)
-    this.state = { didMount: false }
+    this.state = { loaded: false }
   }
 
   componentDidMount() {
-    setTimeout(this.startLoadingAnimation, 0)
+    // TODO: This is just a demo implementation. The real one would be using a flag in redux
+    // that changes when the data has loaded from the smart contract/s
+    setTimeout(this.startLoadingAnimation, 1000)
   }
 
   startLoadingAnimation() {
-    this.setState({ didMount: true })
+    this.setState({ loaded: true })
   }
 
   render() {
-    const { didMount } = this.state
+    const { loaded } = this.state
 
     return (
-      <div
-        className={
-          didMount ? [styles.fade, styles.loaded].join(' ') : styles.fade
-        }
-      >
-        <Categories />
-        <RecentlyAdded />
-        <Footer />
-      </div>
+      <>
+        {loaded && (
+          <>
+            <Categories />
+            <RecentlyAdded />
+            <Footer />
+          </>
+        )}
+
+        {!loaded && <LoadingHome />}
+      </>
     )
   }
 }
