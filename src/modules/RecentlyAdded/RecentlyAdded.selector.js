14905ad89952d106f8af7bd9ff546a36d78d4acd
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-04-12 14:40:49 +0100

    Fix: Make Sure "Recently Added" actually works. #21

--- src/modules/RecentlyAdded/RecentlyAdded.selector.js
+++ src/modules/RecentlyAdded/RecentlyAdded.selector.js
@@ -5,7 +5,16 @@ const getDapps = state => state.dapps
 
 const recentDapps = createSelector(
   [getDapps],
-  dapps => [...dapps].sort((a, b) => moment(b.dateAdded).diff(a.dateAdded)),
+  dapps =>
+    [...dapps].filter(dapp => {
+      return (
+        moment(dapp.dateAdded).toISOString() >
+        moment()
+          .subtract(21, 'days')
+          .utc()
+          .toISOString()
+      )
+    }),
 )
 
 export default recentDapps
