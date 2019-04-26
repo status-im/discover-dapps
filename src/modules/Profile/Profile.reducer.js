cf77606157fee8484be7c139a5759960c825d525
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-04-26 10:10:22 +0100

    Prettify url

--- src/modules/Profile/Profile.reducer.js
+++ src/modules/Profile/Profile.reducer.js
@@ -8,7 +8,13 @@ const MOBILE_NAVIGATE = 'MOBILE_NAVIGATE'
 export const toggleProfileModalAction = dapp => {
   const { innerWidth } = window
   if (innerWidth <= 1024) {
-    history.push(`/${dapp.name.toLowerCase()}`, dapp)
+    history.push(
+      `/${dapp.name
+        .toLowerCase()
+        .trim()
+        .replace(' ', '-')}`,
+      dapp,
+    )
     return {
       type: MOBILE_NAVIGATE,
       payload: dapp,
